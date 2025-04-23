c6e3d5cff70e6d66856400e71085bd6fe29c8f7b
Mubarak Sani <sani.nooridev@gmail.com>
2025-04-23 13:48:38 +0530

    initial commit for the react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Hello World</h3>
+      <p>This React app is deployed on vercel environment</p>
+    </div>
+  );
+}
+
+export default App;
